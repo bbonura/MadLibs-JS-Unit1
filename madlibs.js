65f309152f28c84f902e68525d851b92229fcbd7
Mica <140212633+bbonura@users.noreply.github.com>
2023-07-28 11:27:51 -0400

    Add files via upload

---
+++ madlibs.js
@@ -0,0 +1,11 @@
+
+let place = "Angola";
+let adjective = "soft";
+let lastName = "Johnson";
+let pluralNoun = "mice";
+let adjective2 = "rancid";
+let noun = "car";
+
+let filledStory = "Bonjour! Last week, I went to " + place + ". It was so " + adjective + "! I got to see the " + lastName + " Tower in person, and eat traditional French " + pluralNoun +
+". I would go back to " + place + " again, just to smell the " + adjective2 + " air and walk through the " + noun + " of love once more.";
+console.log(filledStory)
